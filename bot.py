65e0fe4ab2d0335728e828e53981ad6da949a3aa
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-02-14 02:56:18 +0100

    2 new commands with lesson 3

--- bot.py
+++ bot.py
@@ -22,4 +22,33 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f"{member} has left the server")
 
+@client.command()
+async def ping(ctx): #ctx context
+    await ctx.send(f"Pong! {round(client.latency*1000)}ms")
+
+import random
+@client.command(aliases=["8ball"])
+async  def _8ball(ctx, *, question):
+    response = ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(response)}")
+
 client.run(token)
